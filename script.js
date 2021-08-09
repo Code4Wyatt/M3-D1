489903ac1597c6917d8e15fe09e05a536a24e017
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-09 23:01:33 +0100

    Completed homework

--- script.js
+++ script.js
@@ -133,16 +133,51 @@ console.log(mySum)
 Create a function to test whether an array of integers of length 2 contains 1 or a 3. 
 */
 
+const checkArray = function (arr) {
+    if (arr.includes(1) || arr.includes(3)) {
+        return true;
+    } else {
+       return false;
+    }
+};
+
+console.log(checkArray([3,4]))
 /*
 
 10)
 
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
 
+*/
+
+const notIncludes = function (arr) {
+    if(!checkArray(arr)) {
+        return true
+    } else {
+        return false
+    }
+};
+
+
+/*
 11)
 
 Create a function to find the longest string from a given array of strings.
+ */ 
 
+const getLongestStr = function (arr) {
+    const temp = [];
+    for (let i = 0; i < arr.length; i++) {
+        temp.push(arr[i].length);
+    }
+    const largest = [...temp].sort((a, b) => b - a)[0];
+    const index = temp.indexOf(largest);
+    
+    return arr[index];
+};
+
+console.log(getLongestStr(["Small sentence", "Medium sentence", "Largest sentence"]));
+/*
 12)
 
 Create a function to find the types of a given angle.
@@ -153,25 +188,105 @@ Types of angles:
     btuse angle: An angle between 90 and 180 degrees.
     Straight angle: A 180 degree angle.
 
+    */
+
+
+    const angleType = function(angle) {
+        if(angle<90) {
+            return "accute"
+        } else if(angle===90) {
+            return "right angle"
+        } else if(angle<180) {
+            return "obtuse"
+        } else {
+            return "straight"
+        }
+    }
+
+    console.log(angleType(90))
+    /*
+
 13)
 
 Create a function to find the index of the greatest element of a given array of integers
 
+*/
+
+const findIndexOfLargestInt = function(arr) {
+    const largest = [...arr].sort((a,b) => b - a)[0];
+    console.log(largest);
+    return arr.indexOf(largest);
+};
+
+console.log(findIndexOfLargestInt([10, 8 ,24]))
+
+/*
 14)
 
 Create a function to get the largest even number from an array of integers.
 
+*/
+
+const getLargestEven = function(arr) {
+    const temp = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            temp.push(arr[i]);
+        }
+    }
+    return temp.sort((a,b) => b - a)[0];
+};
+
+console.log(getLargestEven([2, 4, 3, 2, 5, 7, 8]));
+/*
+
+15 Create a function to check from two given integers whether one is positive and one is nefative
+
+*/
+const checkSign = function(a,b) {
+    if ((a<0 && b>0) || (a>0 && b<0)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+/*
+
+
+
 16)
-
-Create a function to check from two given integers, whether one is positive and another one is negative.
-
-17)
 
 Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
 
+*/
+
+const convertCase = function(str) {
+    if(str.length < 3) {
+        return str.toUpperCase()
+    } else {
+        return str.slice(0, 3).toLowerCase() + str.slice(3).toUpperCase();
+    }
+};
+
+console.log(convertCase("Strive"));
+/*
 18)
 
 Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+
+*/
+
+const checkSumRange = function(a, b) {
+    if(a+b>50 && a+b<80) {
+        return 65;
+    } else {
+        return 80;
+    }
+};
+
+
+/*
 
 19)
 
@@ -189,7 +304,31 @@ this would be a "DiegoRiccardo".
 34 has four factors: 1, 2, 17, and 34.
 this would be "34".
 
+*/
+
+const numToStringByFactor = function(num) {
+    let str = "";
+    if (num % 3 === 0) str += "Diego";
+    if (num % 5 === 0) str += "Riccardo";
+    if (num % 7 === 0) str += "Stefano";
+    return str;
+};
+
+console.log(numToStringByFactor(21));
+/*
+
 20)
 Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
 
 */
+
+const getAcronym = function (str) {
+    const temp = str.split(" ");
+    let acronym = "";
+    for (let i = 0; i < temp.length; i++) {
+        acronym += temp[i].charAt(0).toUpperCase();
+    }
+    return acronym;
+}
+
+console.log(getAcronym("British Broadcasting Corporation"));
